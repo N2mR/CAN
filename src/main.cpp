5f0ae4245a7257c84068168347d2ba92c3a791ad
Ayumu Nomura <anymmr113.ice@gmail.com>
2023-12-09 18:18:01 +0900

    取得したCanデータをBluetooth経由で送信する機能を追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "CAN_config.h"
 #include "ESP32CAN.h"
 
-#include "CanData.h"
+#include "BluetoothSerial.h"
 
 
 // M5StickC plusのGroveポート
@@ -12,14 +12,17 @@
 
 // CAN設定クラス
 CAN_device_t CAN_cfg;
+// Bluetooth
+BluetoothSerial SerialBT;
 
 // プロトタイプ宣言
-bool getCanData(CanData& objCanData);
-std::string toString_CanData();
+bool existsCanFrame();
+std::string getCanData();
 
 void setup() {
-	// Initialize the M5StickC object
+	// Initialize the M5StickC objects
 	Serial.begin(115200);
+	SerialBT.begin("M5StickC");
 	M5.begin();
 
 	// 画面の向き(0, 1, 2, 3)
@@ -29,69 +32,88 @@ void setup() {
 
 	// CanModule設定
 	CAN_cfg.speed = CAN_SPEED_500KBPS;
-    CAN_cfg.tx_pin_id = TX_PORT;
-    CAN_cfg.rx_pin_id = RX_PORT;
-    CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
-    ESP32Can.CANInit();
+	CAN_cfg.tx_pin_id = TX_PORT;
+	CAN_cfg.rx_pin_id = RX_PORT;
+	CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
+	ESP32Can.CANInit();
 }
 
 void loop() {
-	// CanDataを取得し構造体に詰める
-	CanData objCanData;
-	getCanData(objCanData);
+	// 生存確認
+	M5.Lcd.setCursor(10, 0, 1);
+	std::string running = "running";
+	M5.Lcd.println(running.c_str());
 
 	// CAN通信の状況を表示
-	if (!objCanData.isNull())
+	if (existsCanFrame())
 	{
 		// // CanDataをLCDに出力する
-		M5.Lcd.setCursor(10, 0, 1);
-		M5.Lcd.println(objCanData.strMsgID.c_str());
-		M5.Lcd.setCursor(10, 20, 1);
-		M5.Lcd.println(objCanData.strDLC.c_str());
-		M5.Lcd.setCursor(10, 40, 1);
-		M5.Lcd.println(objCanData.strData.c_str());
+		// M5.Lcd.setCursor(10, 0, 1);
+		// M5.Lcd.println(objCanData.strMsgID.c_str());
+		// M5.Lcd.setCursor(10, 20, 1);
+		// M5.Lcd.println(objCanData.strDLC.c_str());
+		// M5.Lcd.setCursor(10, 40, 1);
+		// M5.Lcd.println(objCanData.strData.c_str());
+
+		// CanDataを文字列取得
+		std::string strOutputCanData = getCanData();
+		// Bluetoothで文字列を送信
+		SerialBT.println(strOutputCanData.c_str());
 	}
-}
-
-/**
- * @details CANのデータを取得
- * 
-*/
-bool getCanData(CanData& objCanData){
-	CAN_frame_t rx_frame;
-	CAN_frame_t tx_frame;
-
-	if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3*portTICK_PERIOD_MS) == pdTRUE) // <- ここでコアダンプする
+	else
 	{
-		// CanDataを作成する
-		
-		// 受信時間
-		objCanData.strTime = std::to_string(millis());
-		// Msg ID
-		objCanData.strMsgID = std::to_string(rx_frame.MsgID);
-		// DLC
-		objCanData.strDLC = std::to_string(rx_frame.FIR.B.DLC);
-		// CAN data
-		std::string strData;
-		for(int i = 0; i < 8; i++)
-		{
-			strData.push_back(rx_frame.data.u8[i]);
-		}
-		objCanData.strData = strData;
+		SerialBT.println("No Signal.");
 	}
-	return true;
 }
 
 /**
- * @details CanDataのtoString
+ * @details CanFrameの存在確認
 */
-std::string toString_CanData(CanData objCanData)
-{	
-	// 出力内容は下記フォーマット
-	// time: xxx, MsgID: xxx, DLC: xxx, Data: xxx
-	std::string strCanOutput = "time: " + objCanData.strTime + " ," + "MsgID: " + objCanData.strMsgID + " ," 
-								+ "DLC: " + objCanData.strDLC + " ," + "Data: " + objCanData.strData;
-	return strCanOutput;
+bool existsCanFrame()
+{
+	CAN_frame_t rx_frame;
+	std::string strMsgID;
+	// メッセージIDが存在する場合はフレームが存在すると判定
+	if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3*portTICK_PERIOD_MS) == pdTRUE)
+	{
+		strMsgID = std::to_string(rx_frame.MsgID);
+	}
+
+	return !strMsgID.empty()? true : false;
 }
 
+std::string getCanData()
+{
+	// CanFrame
+	CAN_frame_t rx_frame;
 
+	if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3*portTICK_PERIOD_MS) == pdTRUE)
+	{
+		// 受信時間
+		std::string strTime = std::to_string(millis());
+		// Msg ID
+		std::string strMsgID = std::to_string(rx_frame.MsgID);
+		// DLC
+		uint8_t DLC = rx_frame.FIR.B.DLC;
+		std::string strDLC = std::to_string(DLC);
+		// CAN data u8
+		std::string strData;
+		for(uint8_t i = 0 ; i < DLC; i++)
+		{
+			int data = static_cast<int>(rx_frame.data.u8[i]);
+			constexpr int bufferSize = sizeof(int) * 2 + 1;  // 整数のバイト数 × 2 + 1（null終端文字列の分）
+			char buffer[bufferSize];
+			
+			// sprintfを使用して整数を16進数文字列に変換
+			sprintf(buffer, "%X", data);
+
+			strData.append(std::string(buffer));
+			strData.append(", ");
+		}
+
+		// 出力文字列の形成
+		std::string strOutputCanData = "time: " + strTime + "  " + "MsgID: " + strMsgID + "  " + "DLC: " + strDLC + "  " + "data: " + strData;
+
+		return strOutputCanData;
+	}
+}
